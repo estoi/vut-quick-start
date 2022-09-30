50d1df54d1b059aa51bb95691df39e12419d3c60
windy <liuguang@kayak.com.cn>
2022-09-30 16:49:06 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from "./router";
 import pinia from "./store";
 
+import "nprogress/nprogress.css";
 import "./styles/index.less";
 
 const app = createApp(App);
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,6 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "virtual:generated-pages";
 import NProgress from "nprogress";
 
-import "nprogress/nprogress.css";
-
-// routes[0].redirect = "/home";
-
 const router = createRouter({
     history: createWebHashHistory(),
     routes
